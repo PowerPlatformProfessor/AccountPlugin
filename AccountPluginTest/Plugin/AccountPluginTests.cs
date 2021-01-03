81c193c5212c8b17473482e425e13b2bee17573e
Danijel Buljat <danijel.buljat@stratiteq.com>
2021-01-03 20:12:01 +0100

    Add some tests

---
+++ AccountPluginTest/Plugin/AccountPluginTests.cs
@@ -0,0 +1,21 @@
+﻿using FakeXrmEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AccountPluginTest.Plugin
+{
+    public class AccountPluginTests
+    {
+        private readonly XrmFakedContext _context;
+
+        [Fact]
+        public void Given_AccountCreated_When_Then_CreateDummyContact()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ AccountPluginTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AccountPluginTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AccountPluginTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ee7b335-838e-4610-8ea1-813c3e421544")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
